a97a62c35671d155eba34babc7f6e6326bc2b289
catcher jones <catchernemail@gmail.com>
2022-09-16 11:21:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/courtcounter1/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.courtcounter1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.courtcounter1.R;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public int scoreA= 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void threepointer(){
+        scoreA+=3;
+        displayForTeamA(scoreA);
+    }
+    public void twopointer(){
+        scoreA+=2;
+
+        displayForTeamA(scoreA);
+    }
+    public void freethrow()
+    {
+        scoreA++;
+        displayForTeamA(scoreA);
+    }
+
+
+    public void displayForTeamA(int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
