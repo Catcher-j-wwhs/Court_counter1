7488c4b3440d1d12a07c9b17026e09a581d4dd93
catcher jones <catchernemail@gmail.com>
2022-09-20 11:49:18 -0500

    part 28

--- app/src/main/java/com/example/courtcounter1/MainActivity.java
+++ app/src/main/java/com/example/courtcounter1/MainActivity.java
@@ -13,6 +13,7 @@ import com.example.courtcounter1.R;
 public class MainActivity extends AppCompatActivity {
 
     public int scoreA= 0;
+    public int scoreB= 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +37,20 @@ public class MainActivity extends AppCompatActivity {
         scoreA++;
         displayForTeamA(scoreA);
     }
+    public void threepointerB(){
+        scoreB+=3;
+        displayForTeamA(scoreB);
+    }
+    public void twopointerB(){
+        scoreB+=2;
+        displayForTeamA(scoreB);
 
+    }
+    public void freethrowB()
+    {
+        scoreB++;
+        displayForTeamA(scoreB);
+    }
 
     public void displayForTeamA(int score){
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
